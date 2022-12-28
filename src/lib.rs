ec30508e66dbc4c436a052754f1168419ad20c1a
Peter Hebden <peterhebden6@gmail.com>
2022-12-28 14:56:38 +0000

    feat: add `bones_camera_shake` crate (#4)
    Adds the camera shake functionality from [Bomby](https://github.com/fishfolk/bomby).
    
    For the time being it uses `bevy_ecs` and not `bones_ecs`.

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,10 @@
 pub mod ecs {
     pub use bones_ecs::*;
 }
+
+/// This crate provides 2D camera shake using the methodology described in this excellent [GDC
+/// talk](https://www.youtube.com/watch?v=tu-Qe66AvtY) by Squirrel Eiserloh.
+#[cfg(feature = "camera_shake")]
+pub mod camera_shake {
+    pub use bones_camera_shake::*;
+}
