b88e4d96297053f0616a878634653fdc85cd3ccb
Zicklag <zicklag@katharostech.com>
2023-01-08 10:59:44 -0600

    feat(bones_input): add `Window` input containing window size.

--- crates/bones_input/src/lib.rs
+++ crates/bones_input/src/lib.rs
@@ -19,3 +19,11 @@ pub struct Time {
     /// The time elapsed since the start of the game session.
     pub elapsed: f32,
 }
+
+/// Information about the window the game is running in.
+#[derive(Clone, Copy, Debug, Default, TypeUlid)]
+#[ulid = "01GP70WMVH4HV4YHZ240E0YC7X"]
+pub struct Window {
+    /// The logical size of the window's client area.
+    pub size: glam::Vec2,
+}
