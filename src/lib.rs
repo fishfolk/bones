d250c7fb83fbddcbdfd7e580394b0864e65119f1
Zicklag <zicklag@katharostech.com>
2023-01-14 10:07:20 -0600

    feat(bones_lib): add camera shake.
    Adds systems and components for easily adding camera shake.
    
    Ported from the Bevy implementation in `bones_camera_shake`.

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub use bones_bevy_utils as bevy_utils;
 /// Bones lib prelude
 pub mod prelude {
     pub use crate::{
-        animation::prelude::*, asset::prelude::*, ecs::prelude::*, input::prelude::*,
+        animation::prelude::*, asset::prelude::*, camera::*, ecs::prelude::*, input::prelude::*,
         render::prelude::*, FrameTime,
     };
 
@@ -19,6 +19,7 @@ pub mod prelude {
 use prelude::*;
 
 pub mod animation;
+pub mod camera;
 
 /// This is a resource that stores the game's fixed frame time.
 ///
@@ -40,4 +41,5 @@ impl Default for FrameTime {
 /// Install the `bones_lib` systems for things such as animation etc. into a [`SystemStages`].
 pub fn install(stages: &mut SystemStages) {
     animation::install(stages);
+    camera::install(stages);
 }
