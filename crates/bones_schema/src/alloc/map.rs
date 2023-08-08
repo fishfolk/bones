087f52154e172454533e9ab8746d46a45de242b9
Zicklag <zicklag@katharostech.com>
2023-08-07 19:45:52 -0500

    feat: finish first draft of SchemaMap.

--- crates/bones_schema/src/alloc/map.rs
+++ crates/bones_schema/src/alloc/map.rs
@@ -1,7 +1,3 @@
-//
-// 🚧 Under Construction 🚧
-//
-
 use std::{
     hash::{BuildHasher, Hasher},
     marker::PhantomData,
@@ -32,7 +28,9 @@ impl SchemaMap {
         }
     }
 
-    /// Insert an item into themap.
+    /// Insert an item into the map.
+    /// # Panics
+    /// Panics if the key or value schemas do not match the map's.
     #[inline]
     #[track_caller]
     pub fn insert<K: HasSchema, V: HasSchema>(&mut self, key: K, value: V) -> Option<V> {
@@ -40,6 +38,8 @@ impl SchemaMap {
     }
 
     /// Insert an item into the map.
+    /// # Errors
+    /// Errors if the key or value schemas do not match the map's.
     pub fn try_insert<K: HasSchema, V: HasSchema>(
         &mut self,
         key: K,
@@ -48,48 +48,63 @@ impl SchemaMap {
         let key = SchemaBox::new(key);
         let value = SchemaBox::new(value);
         self.try_insert_box(key, value)
-            // SOUND: We've try_insert_box won't succeed unless the schema's match, so we have already
-            // checked that the value type matches.
+            // SOUND: try_insert_box won't succeed unless the schema's match, so we have already
+            // checked that the value schema matches.
             .map(|value| value.map(|x| unsafe { x.into_inner_unchecked() }))
     }
 
-    /// Insert an item into the map.
+    /// Insert an untyped item into the map.
+    /// # Panics
+    /// Panics if the key or value schemas do not match the map's.
     #[track_caller]
     #[inline]
     pub fn insert_box(&mut self, key: SchemaBox, value: SchemaBox) -> Option<SchemaBox> {
         self.try_insert_box(key, value).unwrap()
     }
 
-    /// Insert an item into the map.
+    /// Insert an untyped item into the map.
+    /// # Errors
+    /// Errors if the key or value schemas do not match the map's.
     pub fn try_insert_box(
         &mut self,
         key: SchemaBox,
-        mut value: SchemaBox,
+        value: SchemaBox,
     ) -> Result<Option<SchemaBox>, SchemaMismatchError> {
         if key.schema() != self.key_schema || value.schema() != self.value_schema {
             Err(SchemaMismatchError)
         } else {
-            let hash = {
-                let mut hasher = self.map.hasher().build_hasher();
-                hasher.write_u64(key.hash());
-                hasher.finish()
-            };
-            let entry = self
-                .map
-                .raw_entry_mut()
-                .from_hash(hash, |other| other == &key);
-            let previous_value = match entry {
-                bones_utils::hashbrown::hash_map::RawEntryMut::Occupied(mut occupied) => {
-                    std::mem::swap(occupied.get_mut(), &mut value);
-                    Some(value)
-                }
-                bones_utils::hashbrown::hash_map::RawEntryMut::Vacant(vacant) => {
-                    vacant.insert(key, value);
-                    None
-                }
-            };
-
+            // SOUnD: we've checked that the schemas are matching.
+            let previous_value = unsafe { self.insert_box_unchecked(key, value) };
             Ok(previous_value)
+        }
+    }
+
+    /// # Safety
+    /// Key schema and value schema must match the map's.
+    pub unsafe fn insert_box_unchecked(
+        &mut self,
+        key: SchemaBox,
+        mut value: SchemaBox,
+    ) -> Option<SchemaBox> {
+        let hash = {
+            let mut hasher = self.map.hasher().build_hasher();
+            hasher.write_u64(key.hash());
+            hasher.finish()
+        };
+        let entry = self
+            .map
+            .raw_entry_mut()
+            .from_hash(hash, |other| other == &key);
+        // Return the previous value.
+        match entry {
+            bones_utils::hashbrown::hash_map::RawEntryMut::Occupied(mut occupied) => {
+                std::mem::swap(occupied.get_mut(), &mut value);
+                Some(value)
+            }
+            bones_utils::hashbrown::hash_map::RawEntryMut::Vacant(vacant) => {
+                vacant.insert(key, value);
+                None
+            }
         }
     }
 
@@ -113,33 +128,116 @@ impl SchemaMap {
         if K::schema() != self.key_schema || V::schema() != self.value_schema {
             Err(SchemaMismatchError)
         } else {
-            let Some(hash_fn) = self.key_schema.hash_fn else {
-                panic!("Key schema doesn't implement hash");
-            };
-            let Some(eq_fn) = self.key_schema.eq_fn else {
-                panic!("Key schema doesn't implement eq");
-            };
-            let key_ref = key as *const K as *const u8;
-            // SOUND: we know the hash function is valid for the schema
-            let hash = unsafe { (hash_fn)(key_ref) };
-            let hash = {
-                let mut hasher = self.map.hasher().build_hasher();
-                hasher.write_u64(hash);
-                hasher.finish()
-            };
-            let value = self
-                .map
-                .raw_entry()
-                .from_hash(hash, |key| {
-                    let other_ref = key.as_ref().as_ptr();
-                    // SOUND: we know the eq function is valid for the schema
-                    unsafe { (eq_fn)(key_ref, other_ref) }
-                })
-                .map(|x| x.1)
-                // SOUND: we know the schema box's schema matches the casted type.
-                .map(|s_box| unsafe { s_box.as_ref().deref() });
+            // SOUND: we've veriried that they key schema matches the map's
+            let value = unsafe { self.get_ref_unchecked(SchemaRef::new(key)) }
+                // SOUND: we've verified that the value schema maches the map's
+                .map(|x| unsafe { x.deref() });
+
             Ok(value)
         }
+    }
+
+    /// Get an untyped reference to an item in the map.
+    /// # Panics
+    /// Panics if the schema of the key doesn't match.
+    #[inline]
+    #[track_caller]
+    pub fn get_ref(&self, key: SchemaRef) -> Option<SchemaRef> {
+        self.try_get_ref(key).unwrap()
+    }
+
+    /// Get an untyped reference to an item in the map.
+    /// # Errors
+    /// Errors if the schema of the key doesn't match.
+    pub fn try_get_ref(&self, key: SchemaRef) -> Result<Option<SchemaRef>, SchemaMismatchError> {
+        if key.schema() != self.key_schema {
+            Err(SchemaMismatchError)
+        } else {
+            // SOUND: we've check the key schema matches.
+            Ok(unsafe { self.get_ref_unchecked(key) })
+        }
+    }
+
+    /// # Safety
+    /// The key's schema must match this map's key schema.
+    pub unsafe fn get_ref_unchecked(&self, key: SchemaRef) -> Option<SchemaRef> {
+        let Some(hash_fn) = self.key_schema.hash_fn else {
+                panic!("Key schema doesn't implement hash");
+            };
+        let Some(eq_fn) = self.key_schema.eq_fn else {
+                panic!("Key schema doesn't implement eq");
+            };
+        let key_ptr = key.as_ptr();
+        // SOUND: caller asserts the key schema matches
+        let raw_hash = unsafe { (hash_fn)(key_ptr) };
+        let hash = {
+            let mut hasher = self.map.hasher().build_hasher();
+            hasher.write_u64(raw_hash);
+            hasher.finish()
+        };
+        self.map
+            .raw_entry()
+            .from_hash(hash, |key| {
+                let other_ptr = key.as_ref().as_ptr();
+                // SOUND: caller asserts the key schema matches.
+                unsafe { (eq_fn)(key_ptr, other_ptr) }
+            })
+            .map(|x| x.1.as_ref())
+    }
+
+    /// Get an untyped reference to an item in the map.
+    /// # Panics
+    /// Panics if the schema of the key doesn't match.
+    #[inline]
+    #[track_caller]
+    pub fn get_ref_mut(&mut self, key: SchemaRef) -> Option<SchemaRefMut> {
+        self.try_get_ref_mut(key).unwrap()
+    }
+
+    /// Get an untyped reference to an item in the map.
+    /// # Errors
+    /// Errors if the schema of the key doesn't match.
+    pub fn try_get_ref_mut(
+        &mut self,
+        key: SchemaRef,
+    ) -> Result<Option<SchemaRefMut>, SchemaMismatchError> {
+        if key.schema() != self.key_schema {
+            Err(SchemaMismatchError)
+        } else {
+            // SOUND: we've checked that the key schema matches.
+            Ok(unsafe { self.get_ref_unchecked_mut(key) })
+        }
+    }
+
+    /// # Safety
+    /// The key's schema must match this map's key schema.
+    pub unsafe fn get_ref_unchecked_mut(&mut self, key: SchemaRef) -> Option<SchemaRefMut> {
+        let Some(hash_fn) = self.key_schema.hash_fn else {
+            panic!("Key schema doesn't implement hash");
+        };
+        let Some(eq_fn) = self.key_schema.eq_fn else {
+            panic!("Key schema doesn't implement eq");
+        };
+        let key_ptr = key.as_ptr();
+        // SOUND: caller asserts the key schema matches
+        let raw_hash = unsafe { (hash_fn)(key_ptr) };
+        let hash = {
+            let mut hasher = self.map.hasher().build_hasher();
+            hasher.write_u64(raw_hash);
+            hasher.finish()
+        };
+        let entry = self.map.raw_entry_mut().from_hash(hash, |key| {
+            let other_ptr = key.as_ref().as_ptr();
+            // SOUND: caller asserts the key schema matches.
+            unsafe { (eq_fn)(key_ptr, other_ptr) }
+        });
+        match entry {
+            bones_utils::hashbrown::hash_map::RawEntryMut::Occupied(entry) => {
+                Some(entry.into_mut())
+            }
+            bones_utils::hashbrown::hash_map::RawEntryMut::Vacant(_) => None,
+        }
+        .map(|x| x.as_mut())
     }
 
     /// Get a value out of the map for the given key.
@@ -162,40 +260,186 @@ impl SchemaMap {
         if K::schema() != self.key_schema || V::schema() != self.value_schema {
             Err(SchemaMismatchError)
         } else {
-            let Some(hash_fn) = self.key_schema.hash_fn else {
+            // SOUND: we've checked that the key schema matches.
+            let value = unsafe { self.get_ref_unchecked_mut(SchemaRef::new(key)) }
+                // SOUND: we've checked that the value schema matches.
+                .map(|x| unsafe { x.deref_mut() });
+            Ok(value)
+        }
+    }
+
+    /// Remove an item.
+    /// # Panics
+    /// Panics if the schemas of the key and value don't match the map.
+    #[inline]
+    #[track_caller]
+    pub fn remove<K: HasSchema, V: HasSchema>(&mut self, key: &K) -> Option<V> {
+        self.try_remove(key).unwrap()
+    }
+
+    /// Remove an item.
+    /// # Errors
+    /// Errors if the schemas of the key and value don't match the map.
+    pub fn try_remove<K: HasSchema, V: HasSchema>(
+        &mut self,
+        key: &K,
+    ) -> Result<Option<V>, SchemaMismatchError> {
+        if K::schema() != self.key_schema || V::schema() != self.value_schema {
+            Err(SchemaMismatchError)
+        } else {
+            // SOUND: we've checked that the key schema matches.
+            let value = unsafe { self.remove_unchecked(SchemaRef::new(key)) }
+                // SOUND: we've checked that the value schema matches.
+                .map(|x| unsafe { x.into_inner_unchecked() });
+            Ok(value)
+        }
+    }
+
+    /// Untypededly remove an item.
+    /// # Panics
+    /// Panics if the key schema does not match the map's.
+    #[inline]
+    #[track_caller]
+    pub fn remove_box(&mut self, key: SchemaRef) -> Option<SchemaBox> {
+        self.try_remove_box(key).unwrap()
+    }
+
+    /// Untypededly remove an item.
+    /// # Errors
+    /// Errors if the key schema does not match the map's.
+    pub fn try_remove_box(
+        &mut self,
+        key: SchemaRef,
+    ) -> Result<Option<SchemaBox>, SchemaMismatchError> {
+        if key.schema() != self.key_schema {
+            Err(SchemaMismatchError)
+        } else {
+            // SOUND: we've checked the key schema matches.
+            Ok(unsafe { self.remove_unchecked(key) })
+        }
+    }
+
+    /// # Safety
+    /// The key schema must match the map's.
+    pub unsafe fn remove_unchecked(&mut self, key: SchemaRef) -> Option<SchemaBox> {
+        let Some(hash_fn) = self.key_schema.hash_fn else {
                 panic!("Key schema doesn't implement hash");
             };
-            let Some(eq_fn) = self.key_schema.eq_fn else {
+        let Some(eq_fn) = self.key_schema.eq_fn else {
                 panic!("Key schema doesn't implement eq");
             };
-            let key_ref = key as *const K as *const u8;
-            // SOUND: we know the hash function is valid for the schema
-            let hash = unsafe { (hash_fn)(key_ref) };
-            let hash = {
-                let mut hasher = self.map.hasher().build_hasher();
-                hasher.write_u64(hash);
-                hasher.finish()
-            };
-            let entry = self.map.raw_entry_mut().from_hash(hash, |key| {
-                let other_ref = key.as_ref().as_ptr();
-                // SOUND: we know the eq function is valid for the schema
-                unsafe { (eq_fn)(key_ref, other_ref) }
-            });
-            let value = match entry {
-                bones_utils::hashbrown::hash_map::RawEntryMut::Occupied(entry) => {
-                    Some(entry.into_mut())
-                }
-                bones_utils::hashbrown::hash_map::RawEntryMut::Vacant(_) => None,
-            }
-            // SOUND: we know the schema box's schema matches the casted type.
-            .map(|x| unsafe { x.as_mut().deref_mut() });
-            Ok(value)
+        let key_ptr = key.as_ptr();
+        // SOUND: caller asserts the key schema matches
+        let hash = unsafe { (hash_fn)(key_ptr) };
+        let hash = {
+            let mut hasher = self.map.hasher().build_hasher();
+            hasher.write_u64(hash);
+            hasher.finish()
+        };
+        let entry = self.map.raw_entry_mut().from_hash(hash, |key| {
+            let other_ptr = key.as_ref().as_ptr();
+            // SOUND: caller asserts the key schema matches
+            unsafe { (eq_fn)(key_ptr, other_ptr) }
+        });
+        match entry {
+            bones_utils::hashbrown::hash_map::RawEntryMut::Occupied(entry) => Some(entry.remove()),
+            bones_utils::hashbrown::hash_map::RawEntryMut::Vacant(_) => None,
+        }
+    }
+
+    /// Convert into a typed [`SMap`].
+    /// # Panics
+    /// Panics if the schemas of K and V don't match the [`SchemaMap`].
+    #[inline]
+    #[track_caller]
+    pub fn into_smap<K: HasSchema, V: HasSchema>(self) -> SMap<K, V> {
+        self.try_into_smap().unwrap()
+    }
+
+    /// Convert into a typed [`SMap`].
+    /// # Errors
+    /// Errors if the schemas of K and V don't match the [`SchemaMap`].
+    pub fn try_into_smap<K: HasSchema, V: HasSchema>(
+        self,
+    ) -> Result<SMap<K, V>, SchemaMismatchError> {
+        if K::schema() == self.key_schema && V::schema() == self.value_schema {
+            Ok(SMap {
+                map: self,
+                _phantom: PhantomData,
+            })
+        } else {
+            Err(SchemaMismatchError)
         }
     }
 }
 
 /// Typed version of a [`SchemaMap`].
+///
+/// This works essentially like a [`HashMap`], but is compatible with the schema ecosystem.
+///
+/// It is also slightly more efficient to access an [`SMap`] compared to a [`SchemaMap`] because it
+/// doesn't need to do a runtime schema check every time the map is accessed.
 pub struct SMap<K: HasSchema, V: HasSchema> {
-    _map: SchemaMap,
+    map: SchemaMap,
     _phantom: PhantomData<(K, V)>,
+}
+impl<K: HasSchema, V: HasSchema> Default for SMap<K, V> {
+    fn default() -> Self {
+        Self {
+            map: SchemaMap::new(K::schema(), V::schema()),
+            _phantom: Default::default(),
+        }
+    }
+}
+
+impl<K: HasSchema, V: HasSchema> SMap<K, V> {
+    /// Initialize the [`SMap`].
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Insert an entry into the map, returning the previous value, if it exists.
+    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+        // SOUND: schemas are checked to match when SMap is constructed.
+        unsafe {
+            self.map
+                .insert_box_unchecked(SchemaBox::new(k), SchemaBox::new(v))
+                .map(|x| x.into_inner_unchecked())
+        }
+    }
+
+    /// Get a reference to an item in the map.
+    pub fn get(&self, key: &K) -> Option<&V> {
+        // SOUND: schemas are checked to match when SMap is constructed.
+        unsafe {
+            self.map
+                .get_ref_unchecked(SchemaRef::new(key))
+                .map(|x| x.deref())
+        }
+    }
+
+    /// Get a mutable reference to an item in the map.
+    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
+        // SOUND: schemas are checked to match when SMap is constructed.
+        unsafe {
+            self.map
+                .get_ref_unchecked_mut(SchemaRef::new(key))
+                .map(|x| x.deref_mut())
+        }
+    }
+
+    /// Remove an item from the map.
+    pub fn remove(&mut self, key: &K) -> Option<V> {
+        // SOUND: schemas are checked to match when SMap is constructed.
+        unsafe {
+            self.map
+                .remove_unchecked(SchemaRef::new(key))
+                .map(|x| x.into_inner_unchecked())
+        }
+    }
+
+    /// Convert into an untyped [`SchemaMap`].
+    pub fn into_schema_map(self) -> SchemaMap {
+        self.map
+    }
 }
