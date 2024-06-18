41df97502d2ff036752084899e06f1bcfcb97423
Tekhnae Raav <tekhnaeraav@katharostech.com>
2024-06-18 09:11:01 -0500

    Remove Unused RendererApi

--- framework_crates/bones_framework/src/render.rs
+++ framework_crates/bones_framework/src/render.rs
@@ -4,9 +4,7 @@ use bones_lib::prelude::*;
 
 /// Module prelude.
 pub mod prelude {
-    pub use super::{
-        camera::*, color::*, line::*, sprite::*, tilemap::*, transform::*, Renderer, RendererApi,
-    };
+    pub use super::{camera::*, color::*, line::*, sprite::*, tilemap::*, transform::*};
 
     #[cfg(feature = "audio")]
     pub use super::audio::*;
@@ -35,25 +33,3 @@ pub fn render_plugin(session: &mut Session) {
         .install_plugin(ui::ui_plugin)
         .install_plugin(camera::plugin);
 }
-
-/// Trait for the interface exposed by external bones renderers.
-///
-/// These methods allow the game to notify the renderer when certain things happen, and to allow the
-/// game to instruct the renderer to do certain things.
-///
-///
-pub trait RendererApi: Sync + Send {
-    /// Have the renderer delete the session.
-    ///
-    /// The default implementation doesn't do anything, and that may be appropriate for some
-    /// renderers. Other renderers may need to clean up synchronized entities that are present in
-    /// the deleted session.
-    fn delete_session(&self, session: Session) {
-        let _ = session;
-    }
-}
-
-/// Resource containing the [`RendererApi`] implementation provided by the bones renderer.
-#[derive(HasSchema)]
-#[schema(opaque, no_clone, no_default)]
-pub struct Renderer(Box<dyn RendererApi>);
