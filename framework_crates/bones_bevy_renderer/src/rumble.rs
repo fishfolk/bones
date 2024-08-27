236c814cc3c28d092c2f9c8eff7ecb5c979a671d
RockasMockas <6405564+RockasMockas@users.noreply.github.com>
2024-08-27 23:55:07 +0200

    feat: Improving gamepad rumble interface (#447)
    Now supports both adding and setting rumble to provide both options
    natively based on what is needed.

--- framework_crates/bones_bevy_renderer/src/rumble.rs
+++ framework_crates/bones_bevy_renderer/src/rumble.rs
@@ -1,4 +1,3 @@
-use crate::bones::SVec;
 use crate::BonesGame;
 use bevy::input::gamepad::{
     GamepadRumbleIntensity as BevyGamepadRumbleIntensity,
@@ -8,37 +7,61 @@ use bevy::prelude::*;
 use bevy::utils::Duration;
 use bones_framework::prelude as bones;
 
+/// Handles rumble requests from the Bones framework and translates them to Bevy rumble requests
 pub fn handle_bones_rumble(
     game: ResMut<BonesGame>,
     mut rumble_requests: EventWriter<BevyGamepadRumbleRequest>,
 ) {
     if let Some(mut bones_rumble_requests) = game.shared_resource_mut::<bones::GamepadsRumble>() {
-        for request in &bones_rumble_requests.requests {
+        while let Some(request) = bones_rumble_requests.requests.pop_front() {
             match request {
-                bones::GamepadRumbleRequest::Trigger {
+                bones::GamepadRumbleRequest::AddRumble {
                     gamepad,
                     intensity,
                     duration,
                 } => {
-                    let bevy_intensity = BevyGamepadRumbleIntensity {
-                        strong_motor: intensity.strong_motor(),
-                        weak_motor: intensity.weak_motor(),
-                    };
-
-                    let gamepad = Gamepad::new(*gamepad as usize);
-
-                    rumble_requests.send(BevyGamepadRumbleRequest::Add {
-                        gamepad,
-                        intensity: bevy_intensity,
-                        duration: Duration::from_secs_f32(*duration),
+                    send_rumble_request(&gamepad, &intensity, &duration, &mut rumble_requests);
+                }
+                bones::GamepadRumbleRequest::SetRumble {
+                    gamepad,
+                    intensity,
+                    duration,
+                } => {
+                    // First, stop the current rumble
+                    let stop_gamepad = Gamepad::new(gamepad as usize);
+                    rumble_requests.send(BevyGamepadRumbleRequest::Stop {
+                        gamepad: stop_gamepad,
                     });
+
+                    // Then, add the new rumble
+                    send_rumble_request(&gamepad, &intensity, &duration, &mut rumble_requests);
                 }
                 bones::GamepadRumbleRequest::Stop { gamepad } => {
-                    let gamepad = Gamepad::new(*gamepad as usize);
+                    let gamepad = Gamepad::new(gamepad as usize);
                     rumble_requests.send(BevyGamepadRumbleRequest::Stop { gamepad });
                 }
             }
         }
-        bones_rumble_requests.requests = SVec::new();
     }
+}
+
+/// Helper function to send a rumble request to Bevy
+fn send_rumble_request(
+    gamepad: &u32,
+    intensity: &bones::GamepadRumbleIntensity,
+    duration: &f32,
+    rumble_requests: &mut EventWriter<BevyGamepadRumbleRequest>,
+) {
+    let bevy_intensity = BevyGamepadRumbleIntensity {
+        strong_motor: intensity.strong_motor(),
+        weak_motor: intensity.weak_motor(),
+    };
+
+    let gamepad = Gamepad::new(*gamepad as usize);
+
+    rumble_requests.send(BevyGamepadRumbleRequest::Add {
+        gamepad,
+        intensity: bevy_intensity,
+        duration: Duration::from_secs_f32(*duration),
+    });
 }
