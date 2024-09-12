0df9991a908520d92db5ba01d25f646beee6de8c
Max Whitehead <max.blackllama.alt3@gmail.com>
2024-09-11 20:15:32 -0700

    feat: DesyncHash derive macro

--- framework_crates/bones_utils/macros/src/lib.rs
+++ framework_crates/bones_utils/macros/src/lib.rs
@@ -1,5 +1,7 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote, quote_spanned, spanned::Spanned};
+use venial::StructFields;
 
 /// Helper macro to bail out of the macro with a compile error.
 macro_rules! throw {
@@ -167,4 +169,143 @@ pub fn derive_deref_mut(input: TokenStream) -> TokenStream {
     } else {
         throw!(input, "Cannot derive DerefMut on anything but structs.");
     }
+}
+
+#[proc_macro_derive(DesyncHash, attributes(desync_hash_module))]
+pub fn derive_desync_hash(input: TokenStream) -> TokenStream {
+    let input = venial::parse_declaration(input.into()).unwrap();
+    let name = input.name().expect("Type must have a name");
+
+    // Get the schema module, reading optionally from the `schema_module` attribute, so that we can
+    // set the module to `crate` when we want to use it within the `bones_schema` crate itself.
+    let desync_hash_module = input
+        .attributes()
+        .iter()
+        .find_map(|attr| {
+            (attr.path.len() == 1 && attr.path[0].to_string() == "desync_hash_module").then(|| {
+                attr.value
+                    .get_value_tokens()
+                    .iter()
+                    .cloned()
+                    .collect::<TokenStream2>()
+            })
+        })
+        .unwrap_or_else(|| quote!(bones_utils));
+
+    // Helper to get hash invocations of struct fields
+    let hash_struct_fields = |fields: &StructFields| match fields {
+        venial::StructFields::Tuple(tuple) => tuple
+            .fields
+            .iter()
+            .enumerate()
+            .map(|(idx, (field, _))| {
+                let ty = &field.ty;
+                quote! {<#ty as #desync_hash_module::DesyncHash>::hash(&self.#idx, hasher);}
+            })
+            .collect::<Vec<_>>(),
+        venial::StructFields::Named(named) => named
+            .fields
+            .iter()
+            .map(|(field, _)| {
+                let name = &field.name;
+                let ty = &field.ty;
+
+                quote! {<#ty as #desync_hash_module::DesyncHash>::hash(&self.#name, hasher);}
+            })
+            .collect::<Vec<_>>(),
+        venial::StructFields::Unit => vec![],
+    };
+
+    // Get fields of enum variant
+    let enum_variant_fields = |fields: &StructFields| match fields {
+        venial::StructFields::Tuple(tuple) => {
+            // Build identifiers for tuple as  as a,b,c...
+
+            if tuple.fields.len() > 26 {
+                panic!("DesyncHash derive macro does not support variants of tuples with more than 26 fields.");
+            }
+            let identifiers: Vec<_> = (0..tuple.fields.len())
+                .map(|i| format_ident!("{}", (b'a' + i as u8) as char))
+                .collect();
+
+            // format as (a, b, c, ...)
+            let tuple_fields = quote! {
+                (#(#identifiers),*)
+            };
+
+            // generate invocations for each field in tuple using generated identifier
+            let invocations = identifiers
+                .iter()
+                .map(|ident| {
+                    quote! {#desync_hash_module::DesyncHash::hash(#ident, hasher);}
+                })
+                .collect::<Vec<_>>();
+            (tuple_fields, invocations)
+        }
+        venial::StructFields::Named(named) => {
+            let field_idents: Vec<_> = named.fields.iter().map(|f| &f.0.name).collect();
+
+            // format list of fields as '{ fieldA, fieldB, ... }'
+            let named_fields = quote! {
+                {#(#field_idents),*}
+            };
+
+            let invocations = field_idents
+                .iter()
+                .map(|ident| {
+                    quote! {#desync_hash_module::DesyncHash::hash(#ident, hasher);}
+                })
+                .collect::<Vec<_>>();
+            (named_fields, invocations)
+        }
+        venial::StructFields::Unit => (quote! {}, vec![]),
+    };
+
+    let field_hash_invocations: Vec<_> = match &input {
+        venial::Declaration::Struct(s) => hash_struct_fields(&s.fields),
+        venial::Declaration::Enum(e) => {
+            let mut variants = Vec::new();
+
+            let enum_name = &e.name;
+            for (idx, v) in e.variants.items().enumerate() {
+                let variant_name = &v.name;
+                let (variant_fields_string, invocations) = enum_variant_fields(&v.contents);
+                variants.push(quote! {
+                    #enum_name::#variant_name #variant_fields_string => {
+                        // Hash index of variant to ensure that two variants are unique
+                        hasher.write_usize(#idx);
+                        #(
+                            #invocations
+                        )*
+                    },
+                });
+            }
+
+            vec![quote! {
+                match self {
+                    #(#variants)*
+                }
+            }]
+        }
+        venial::Declaration::Union(_) => {
+            panic!("DesyncHash derive macro impl does not support Unions");
+        }
+        _ => vec![],
+    };
+
+    let combined_hash_invocations = field_hash_invocations.iter().fold(quote! {}, |acc, q| {
+        quote! {
+            #acc
+            #q
+        }
+    });
+
+    quote! {
+        impl #desync_hash_module::DesyncHash for #name {
+            fn hash(&self, hasher: &mut dyn std::hash::Hasher) {
+                #combined_hash_invocations
+            }
+        }
+    }
+    .into()
 }
